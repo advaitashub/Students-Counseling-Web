87c88b90fcf39b9271aca47eefc978bb5c7d5fe9
advaitashub <singhadvaita95@gmail.com>
2025-12-17 20:51:30 +0530

    statcfiles img folder

--- student_coun_webapp/settings.py
+++ student_coun_webapp/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # folder where collectstatic will put files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
