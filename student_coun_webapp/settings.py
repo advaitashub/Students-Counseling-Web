47d4ad93eeca4d07da92f27c83723ad0f08e0142
advaitashub <singhadvaita95@gmail.com>
2025-12-19 11:53:59 +0530

    Use SendGrid for email

--- student_coun_webapp/settings.py
+++ student_coun_webapp/settings.py
@@ -186,6 +186,8 @@ DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL",
     "studentwebapp2025@gmail.com"
 )
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+
 
 # EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
