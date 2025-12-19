387b8c06fae51ffc1a3260f629593e765c3d764a
advaitashub <singhadvaita95@gmail.com>
2025-12-19 10:26:58 +0530

    email formatting

--- home/urls.py
+++ home/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path(
         'registration/password_reset/',
         auth_views.PasswordResetView.as_view(
-            template_name='registration/password_reset.html',
+            template_name='registration/password_reset_form.html',
             email_template_name='registration/custom_password_reset_email.html',
             extra_email_context={
                 'domain': settings.DOMAIN,
